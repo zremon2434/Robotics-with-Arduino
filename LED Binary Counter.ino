3340b1b596c25ef99ff658b046e921ce016adef2
ZR Emon <140493256+zremon2434@users.noreply.github.com>
2023-07-31 11:08:39 -0700

    Add files via upload

---
+++ LED Binary Counter.ino
@@ -0,0 +1,247 @@
+int one=13;
+int two=12;
+int three=8;
+int four=4;
+int dah=2000;
+
+
+void setup()
+{
+pinMode(one,OUTPUT);
+pinMode(two,OUTPUT);
+pinMode(three,OUTPUT);
+pinMode(four,OUTPUT);
+  
+  
+  
+  
+}
+
+void loop()
+{
+
+  digitalWrite(one,LOW);
+  
+   digitalWrite(two,LOW);
+  
+   digitalWrite(three,LOW);
+  
+   digitalWrite(four,LOW);
+  
+  delay(dah);
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+
+   digitalWrite(two,LOW);
+ 
+   digitalWrite(three,LOW);
+  
+   digitalWrite(four,HIGH);
+  delay(dah);
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+  
+   digitalWrite(two,LOW);
+  
+   digitalWrite(three,HIGH);
+ 
+   digitalWrite(four,LOW);
+
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+ 
+   digitalWrite(two,LOW);
+  
+   digitalWrite(three,HIGH);
+  
+   digitalWrite(four,HIGH);
+ 
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+
+   digitalWrite(two,HIGH);
+  
+   digitalWrite(three,LOW);
+ 
+   digitalWrite(four,LOW);
+ 
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+ 
+   digitalWrite(two,HIGH);
+ 
+   digitalWrite(three,LOW);
+ 
+   digitalWrite(four,HIGH);
+
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+
+   digitalWrite(two,HIGH);
+  
+   digitalWrite(three,HIGH);
+  
+   digitalWrite(four,LOW);
+ 
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,LOW);
+
+   digitalWrite(two,HIGH);
+
+   digitalWrite(three,HIGH);
+
+   digitalWrite(four,HIGH);
+ 
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+ 
+   digitalWrite(two,LOW);
+
+   digitalWrite(three,LOW);
+  
+   digitalWrite(four,LOW);
+
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+
+   digitalWrite(two,LOW);
+ 
+   digitalWrite(three,LOW);
+ 
+   digitalWrite(four,HIGH);
+
+  delay(dah);
+  
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+
+   digitalWrite(two,LOW);
+
+   digitalWrite(three,HIGH);
+ 
+   digitalWrite(four,LOW);
+ 
+  delay(dah);
+  
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+ 
+   digitalWrite(two,LOW);
+ 
+   digitalWrite(three,HIGH);
+ 
+   digitalWrite(four,HIGH);
+  
+  delay(dah);
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+ 
+   digitalWrite(two,HIGH);
+  
+   digitalWrite(three,LOW);
+
+   digitalWrite(four,LOW);
+
+  delay(dah);
+  
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+ 
+   digitalWrite(two,HIGH);
+
+   digitalWrite(three,LOW);
+ 
+   digitalWrite(four,HIGH);
+
+  delay(dah);
+  
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+
+   digitalWrite(two,HIGH);
+ 
+   digitalWrite(three,HIGH);
+  
+   digitalWrite(four,LOW);
+ 
+  delay(dah);
+  
+  
+  
+  
+  
+  
+  digitalWrite(one,HIGH);
+ 
+   digitalWrite(two,HIGH);
+
+   digitalWrite(three,HIGH);
+
+   digitalWrite(four,HIGH);
+  
+  delay(dah);
+  
+  
+}          
+  
