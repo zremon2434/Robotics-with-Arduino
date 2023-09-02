4cd7ab2a0654dc954a495f02bf627c0ced71cb7d
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-09-03 00:43:37 +0600

    Add files via upload

---
+++ Arduino For loop(EmoMech).ino
@@ -0,0 +1,33 @@
+int pin1=6;
+int delayt=2000;
+int delaytt=1000;
+int pin2=9;
+int pin1blink=3;
+int pin2blink=5;
+int j;
+
+void setup()
+{
+  pinMode(pin1,OUTPUT);
+  
+  pinMode(pin2,OUTPUT);
+        
+}
+
+void loop()
+{
+
+  for(j=1;j<=pin1blink;j=j+1){digitalWrite(pin1,HIGH);
+  delay(delaytt);
+  digitalWrite(pin1,LOW);
+  delay(delaytt);}
+
+  delay(delayt);
+    
+  for(j=1;j<=pin2blink;j=j+1){digitalWrite(pin2,HIGH);
+  delay(delaytt);
+  digitalWrite(pin2,LOW);
+   delay(delaytt);}
+  
+   delay(delayt);  
+}
