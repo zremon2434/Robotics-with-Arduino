7884bee3107ff0c8eab8f933d778fe0831b4abb0
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-10-17 00:32:30 +0600

    Add files via upload

---
+++ pushbutton in Arduino.ino
@@ -0,0 +1,28 @@
+int ledpin=8;
+int buttonpin=12;
+int buttonread;
+int delT=200;
+
+void setup()
+{
+  
+ Serial.begin(9600);
+ pinMode(ledpin,OUTPUT);
+ pinMode(buttonpin,INPUT);
+  
+}
+
+void loop()
+{
+
+ buttonread=digitalRead(buttonpin);
+ Serial.println(buttonread);
+  
+  if(buttonread==1){
+    digitalWrite(ledpin,LOW);
+  }
+  if(buttonread==0){
+    digitalWrite(ledpin,HIGH);
+  } 
+  
+}
